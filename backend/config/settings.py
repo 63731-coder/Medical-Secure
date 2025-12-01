ca0b862178e618fb4a60fe0844a609888020129d
Christophe ANTAR <62642@etu.he2b.be>
2025-12-01 12:49:12 +0100

    add PostgreSQL config and DRF serializers

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'medical_secure',
+        'USER': 'medical_user',
+        'PASSWORD': 'secure123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
